bef057bf1d36ff8c8419be96e59bc61ec1f00851
xBach03 <dangxuanbach03@gmail.com>
2024-05-30 10:03:43 +0700

    104. maximum depth of binary tree

--- FirstProject/src/MaximumDepth.java
+++ FirstProject/src/MaximumDepth.java
@@ -1,17 +1,17 @@
 public class MaximumDepth {
     public static class TreeNode {
         int val;
-        InvertBinaryTree.TreeNode left;
-        InvertBinaryTree.TreeNode right;
+        TreeNode left;
+        TreeNode right;
         TreeNode() {}
         TreeNode(int val) { this.val = val; }
-        TreeNode(int val, InvertBinaryTree.TreeNode left, InvertBinaryTree.TreeNode right) {
+        TreeNode(int val, TreeNode left, TreeNode right) {
             this.val = val;
             this.left = left;
             this.right = right;
         }
     }
-    public static void print(InvertBinaryTree.TreeNode root) {
+    public static void print(TreeNode root) {
         if(root == null) {
             return;
         }
@@ -19,7 +19,20 @@ public class MaximumDepth {
         print(root.left);
         print(root.right);
     }
-    public int maxDepth(TreeNode root) {
-
+    public static int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        int maxLeft = maxDepth(root.left);
+        int maxRight = maxDepth(root.right);
+        return Math.max(maxLeft, maxRight) + 1;
+    }
+    public static void main(String[] Args) {
+        TreeNode n1 = new TreeNode(15);
+        TreeNode n2 = new TreeNode(7);
+        TreeNode n3 = new TreeNode(9);
+        TreeNode n4 = new TreeNode(20, n1, n2);
+        TreeNode n5 = new TreeNode(3, n3, n4);
+        System.out.println(maxDepth(n5));
     }
 }
